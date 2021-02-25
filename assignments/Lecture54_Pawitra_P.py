808849916d3f9036a8207e98eef3742226fa89de
pawitra <pawitra.p@ku.th>
2021-02-25 20:41:02 +0700

    Create Lecture54_Pawitra_P.py

---
+++ assignments/Lecture54_Pawitra_P.py
@@ -0,0 +1,37 @@
+def login():
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if usernameInput == "admin" and passwordInput == "1234":
+        return True
+    else:
+        return False
+
+def showMenu():
+    print("----- iShop -----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+
+def menuSelect():
+    userSelected = int(input(">> "))
+    return userSelected
+
+def vatCalculator(totalPrice):
+    vat = 7
+    result = totalPrice + (totalPrice * vat / 100)
+    return result
+
+def priceCalculator():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    return vatCalculator(price1 + price2)
+
+if(login()):
+    showMenu()
+    menu = menuSelect()
+    if menu == 1:
+        price = int(input("Price : "))
+        print(vatCalculator(price))
+    else:
+        print(priceCalculator())
+else:
+    print("Username/Password Incorrect.")
