f6b844dc1dba5ec165fa851c34329e246a5a2a7d
pawitra <pawitra.p@ku.th>
2021-02-25 12:03:17 +0700

    Exercise 5 : ปฏิบัติการพัฒนาโปรแกรมที่ใช้งานได้จริง และ ข้อมูล

---
+++ assignments/Exercise5_1_Pawitra_P.py
@@ -0,0 +1,6 @@
+num1 = int(input("1st number:"))
+num2 = int(input("2nd number:"))
+print(num1, "+", num2, "=", num1+num2)
+print(num1, "-", num2, "=", num1-num2)
+print(num1, "*", num2, "=", num1*num2)
+print(num1, "/", num2, "=", num1/num2)
---
+++ assignments/Exercise5_2_Pawitra_P.py
@@ -0,0 +1,4 @@
+s = float(input("Distance: "))
+t = float(input("Time: "))
+v = s/t
+print("Velocity (v=s/t):", v, "km/h")
