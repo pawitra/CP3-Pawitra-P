20010c4906a79ed958793e37d4b2f969e4b95cc7
pawitra <pawitra.p@ku.th>
2021-02-25 11:42:54 +0700

    Create Exercise4_Pawitra_P.py
    Exercise 4 : ปฏิบัติการทดสอบการใช้งานตัวแปร

---
+++ assignments/Exercise4_Pawitra_P.py
@@ -0,0 +1,10 @@
+eng = 97.30
+bus = 56.34
+comSci = 70.00
+comPro = 89.77
+
+print("\n--- Your Score ---\n")
+print("Foundation English :", eng, "\n")
+print("General Business :", bus, "\n")
+print("Introduction to Computer Systems :", comSci, "\n")
+print("Computer Programming :", comPro, "\n")
